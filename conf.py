b34178ed06e2106faea10275b49cb07a1a08633c
Lukasz Rojek <lukasz.rojek.mail@gmail.com>
2022-07-29 13:13:44 +0200

    template commit

---
+++ conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Python für Vermessungsingenieure'
+copyright = '2022, Prof. Dr. Werner Stempfhuber, Lukasz Rojek'
+author = 'Prof. Dr. Werner Stempfhuber, Lukasz Rojek'
+release = '01.01.2022'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+language = 'de'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ index.rst
@@ -0,0 +1,20 @@
+.. Python für Vermessungsingenieure documentation master file, created by
+   sphinx-quickstart on Fri Jul 29 11:03:03 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Python für Vermessungsingenieure's documentation!
+============================================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
